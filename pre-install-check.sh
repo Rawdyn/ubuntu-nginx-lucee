74fd90e6a172b146f377d58efa689382bdd7588d
Rawdyn <github@codebase.com.au>
2020-10-29 10:20:57 +1100

    #2er743 - Updates format of install checks

--- pre-install-check.sh
+++ pre-install-check.sh
@@ -1,71 +1,83 @@
 #!/bin/bash
 
+red="\e[38;5;197m"
+gry="\e[37m"
+wht="\e[38;5;255m"
+blu="\e[38;5;38m"
+grn="\e[38;5;118m"
+ylw="\e[38;5;185m"
+rst="\e[0m"
 # If no Lucee version specified, set default.
 if [[ -z "${LUCEE_VERSION}"  ]]; then
 	export LUCEE_VERSION="5.3.6.61"
 fi
-echo -e "\e[38;5;255m\n\n\n\n============================================================\n"
-echo -e "               INSTALLING LUCEE \e[38;5;38m$LUCEE_VERSION\e[0m"
-echo -e "\e[38;5;255m\n============================================================\n\e[0m"
+echo -e "${wht}\n\n\n\n============================================================\n"
+echo -e "               INSTALLING LUCEE ${blu}$LUCEE_VERSION${rst}"
+echo -e "${wht}\n============================================================\n${rst}"
 
 #If installing the light versio of Lucee
 if [[ -n "${LUCEE_LIGHT}"  ]]; then
-	echo -e "\e[37mInstalling Lucee \e[38;5;38mLight\e[0m"
+	echo -e "${gry}Installing Lucee ${blu}Light${rst}"
 fi
 
 #If a SHA256 provided, note we will verify against it
 if [[ -n "${LUCEE_JAR_SHA256}"  ]]; then
-	echo -e "\e[37mVerify Lucee against SHA256\e[0m"
-	echo -e "\e[38;5;38m$LUCEE_JAR_SHA256\e[0m"
+	echo -e "${gry}Verify Lucee against SHA256${rst}"
+	echo -e "${blu}$LUCEE_JAR_SHA256${rst}"
 fi
 
 #Default heap size if none specified
 if [[ -z "${JVM_MAX_HEAP_SIZE}"  ]]; then
 	export JVM_MAX_HEAP_SIZE="512m"
 fi
-echo -e "\e[37mJVM Max Heap Size will be set to \e[38;5;38m$JVM_MAX_HEAP_SIZE\e[0m"
+echo -e "${gry}JVM Max Heap Size will be set to ${blu}$JVM_MAX_HEAP_SIZE${rst}"
 
+
+if [[ -n "${WHITELIST_IP}"  ]]; then
+	echo -e "${gry}Will grant ${blu}$WHITELIST_IP${gry} access to /lucee${rst}"
+fi
+
+if [[ -z "${ADMIN_PASSWORD}"  ]]; then
+	echo -e "${gry}No ADMIN_PASSWORD set${rst}"
+else
+	echo -e "${gry}CFAdmin password will be set to ${blu}$ADMIN_PASSWORD${rst}"
+fi
 
 #if EITHER JVM file or version specified
 if [[ -n "${JVM_FILE}"  ]] || [[ -n "${JVM_VERSION}"  ]] ; then
 	#if JVM_FILE requested
-	f_msg="Requested custom JVM File...\n\e[38;5;38m   $JVM_FILE\e[37m\n   The custom JVM file was"
+	fv_error=false
+	f_msg="Requested custom JVM File...\n${blu}   $JVM_FILE${gry}\n   The custom JVM file was"
 	if [[ -n "${JVM_FILE}"  ]]; then
 		#if JFM_FILE exists
 		if [ -f $JVM_FILE ]; then
-			f_msg+="\e[92m found\e[0m."
+			f_msg+="${grn} found${rst}."
 		else
-			f_msg+="\e[38;5;197m not found\e[0m."
+			f_msg+="${red} not found${rst}."
+			fv_error=true
 		fi
 		echo -e "$f_msg"
 	fi
-	#if JVM_VERSION specified	
-	if [[ -n "${JVM_VERSION}"  ]]; then	
-		echo -e "   \e[37mJVM Version '\e[38;5;38m$JVM_VERSION\e[37m'\e[38;5;185m CHECK\e[37m it corresponds with file.\e[0m"
+	#if JVM_VERSION specified
+	if [[ "$fv_error" == false ]]; then
+		if [[ -n "${JVM_VERSION}"  ]]; then	
+			echo -e "   ${gry}JVM Version '${blu}$JVM_VERSION${gry}'${ylw} CHECK${gry} it corresponds with file.${rst}"
+		else
+			echo -e "   ${red}ALERT! ${gry}Expected JVM Version not set.${rst}"
+			fv_error=true
+		fi
+	fi
+	if [[ "$fv_error" == false ]]; then
+		echo -e "   \e[38;5;118mOK${gry} to use custom JVM file and version specified.${rst}"
 	else
-		echo -e "   \e[38;5;197mALERT! \e[37mExpected JVM Version not set.\e[0m"
-	fi	
-	if [[ -n "${JVM_FILE}"  ]] && [[ -n "${JVM_VERSION}"  ]] && [ -f $JVM_FILE ]; then
-		echo -e "   \e[38;5;118mOK\e[37m to use custom JVM file and version specified.\e[0m"
-	else
-		echo -e "   \e[38;5;197mALERT!\e[37m Could not resolve custom JVM file and version.\e[0m"
-		echo -e "   \e[38;5;197mALERT!\e[37m Default OpenJDK will be used if you proceed.\e[0m"
+		echo -e "   ${red}ALERT!${gry} Could not resolve custom JVM file and version.${rst}"
+		echo -e "   ${red}ALERT!${gry} Default OpenJDK will be used if you proceed.${rst}"
 	fi	
 else
 	#As neither JVM_FILE nor JVM_VERSION were specified...
-	echo -e "\e[37mNo JVM options specified so skipping custom JVM Installation.\e[0m"
+	echo -e "${gry}No JVM options specified so skipping custom JVM Installation.${rst}"
 fi
 
-
-if [[ -n "${WHITELIST_IP}"  ]]; then
-	echo -e "\e[37mWill grant \e[38;5;38m$WHITELIST_IP\e[37m access to /lucee\e[0m"
-fi
-
-if [[ -z "${ADMIN_PASSWORD}"  ]]; then
-	echo -e "\e[37mNo ADMIN_PASSWORD set\e[0m"
-else
-	echo -e "\e[37mCFAdmin password will be set to \e[38;5;38m$ADMIN_PASSWORD\e[0m"
-fi
 
 #root permission check
 if [ "$(whoami)" != "root" ]; then
@@ -74,14 +86,18 @@ if [ "$(whoami)" != "root" ]; then
 fi
 
 #Confirm to continue with install
-echo -e "\e[92m"
+if [[ "$fv_error" == false ]]; then
+	echo -e "${grn}"
+else
+	echo -e "${red}"
+fi
 while true; do
     read -p "Do you wish to proceed with this installation (y/n)? " yn
     case $yn in
-        [Yy]* ) echo -e "\n\e[38;5;118mOK, let's do it!\e[0m"; break;;
-        [Nn]* ) echo -e "\n\e[38;5;197mExiting without installing.\e[0m\n"; exit;;
+        [Yy]* ) echo -e "\n${grn}OK, let's do it!${rst}"; break;;
+        [Nn]* ) echo -e "\n${red}Exiting without installing.${rst}\n"; exit;;
         * ) ;;
     esac
 done
-echo -e "\e[0m"
-echo -e "\e[38;5;118mRunning Installation of Lucee $LUCEE_VERSION \e[0m\n"
+echo -e "${rst}"
+echo -e "${grn}Running Installation of Lucee $LUCEE_VERSION ${rst}\n"
