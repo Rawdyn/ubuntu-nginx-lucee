d0e528889775c0df4b0b73e3eb00b84fb896afa1
Rawdyn <github@codebase.com.au>
2020-10-29 17:16:46 +1100

    #2er743 - Alters JVM to extract tar in named version folder

--- scripts/400-jvm.sh
+++ scripts/400-jvm.sh
@@ -2,14 +2,14 @@
 
 echo "Installing Oracle JVM"
 if [[ -n "${JVM_FILE}"  ]] && [ -f $JVM_FILE ]; then
-  mkdir /opt/lucee/jvm
-  tar -xf $JVM_FILE -C /opt/lucee/jvm
-  chown -R root:root /opt/lucee/jvm
-  chmod -R 755 /opt/lucee/jvm
-  ln -s /opt/lucee/jvm/jdk$JVM_VERSION /opt/lucee/jvm/current
-  echo $'\nJAVA_HOME="/opt/lucee/jvm/current"' >> /etc/default/tomcat8
+	mkdir -p /opt/lucee/jvm/$JVM_VERSION
+	tar -xzf $JVM_FILE -C /opt/lucee/jvm/$JVM_VERSION --strip-components=1
+	chown -R root:root /opt/lucee/jvm
+	chmod -R 755 /opt/lucee/jvm
+	ln -s /opt/lucee/jvm/$JVM_VERSION /opt/lucee/jvm/current
+	echo $'\nJAVA_HOME="/opt/lucee/jvm/current"' >> /etc/default/tomcat8
 else
-  echo "File $JVM_FILE not found, SKIPPING Oracle JVM Installation"
+	echo "File $JVM_FILE not found, SKIPPING Oracle JVM Installation"
 fi
 
 echo "Tomcat / Lucee Configuration Done, Restarting Tomcat"
